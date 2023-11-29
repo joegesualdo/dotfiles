1518b03c0abd181ff8fd0d26efb87a49a1b0bcc6
joegesualdo <joegesualdo@gmail.com>
2023-11-28 22:12:43 -0500

    Add more configs

--- install.sh
+++ install.sh
@@ -1,1 +1,4 @@
 ln -s nvim ~/.config/nvim
+ln -s nvim ~/.config/tmux
+ln -s nvim ~/.config/wezterm
+ln -s  zshrc ~/.zshrc
---
+++ wezterm/wezterm.lua
@@ -0,0 +1,32 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = "Gruvbox Dark (Gogh)"
+
+config.font = wezterm.font("Monaco", { weight = "Regular" })
+config.font_size = 12
+
+-- Remove border that shows when using vim
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
+
+config.audible_bell = "Disabled" -- disable the bell sound
+
+-- and finally, return the configuration to wezterm
+return config
