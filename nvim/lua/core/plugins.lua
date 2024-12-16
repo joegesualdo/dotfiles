61c2c84daf203b7e456e75faa05034e67c6b3618
Joe Gesualdo <joegesualdo@gmail.com>
2024-12-15 22:16:52 -0500

    Add codium

--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -90,5 +90,28 @@ require("lazy").setup({
             require("core.plugin_config.lsp_general") -- Load custom configuration
         end,
     },
+    {
+      'Exafunction/codeium.vim',
+      event = 'BufEnter',
+      config = function()
+        -- this is how you toggle codeium
+        vim.g.codeium_enabled = true
+        vim.g.codeium_disable_bindings = 1
+
+        -- Change '<C-g>' here to any keycode you like.
+        vim.keymap.set("i", "<C-g>", function()
+            return vim.fn["codeium#Accept"]()
+        end, { expr = true })
+        vim.keymap.set("i", "<C-;>", function()
+            return vim.fn["codeium#CycleCompletions"](1)
+        end, { expr = true })
+        vim.keymap.set("i", "<C-,>", function()
+            return vim.fn["codeium#CycleCompletions"](-1)
+        end, { expr = true })
+        vim.keymap.set("i", "<C-x>", function()
+            return vim.fn["codeium#Clear"]()
+        end, { expr = true })
+      end
+    }
 })
 -- END Lazy plugin manager
